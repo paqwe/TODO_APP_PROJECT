bd945c6d12eca6ddcae840e0048dabbf58bc6dbc
prashanta das <106626017+paqwe@users.noreply.github.com>
2024-06-26 19:39:12 +0530

    commit

---
+++ app.py
@@ -0,0 +1,36 @@
+from flask import Flask, render_template,request,redirect, url_for
+from pymongo import MongoClient
+from bson.objectid import ObjectId
+
+
+app =Flask(__name__)
+
+client = MongoClient('localhost', 27017)
+
+@app.route("/",methods=['GET','POST'])
+def index():
+    if request.method == "POST":   
+        content = request.form['content']
+        degree = request.form['degree']
+        todos.insert_one({'content': content, 'degree': degree})
+        return redirect(url_for('index'))
+    all_todos = todos.find()
+    return render_template('index.htm',todos=all_todos)
+
+
+@app.post("/<id>/delete/")
+def delete(id):
+    todos.delete_one({"_id":ObjectId(id)})
+    return redirect(url_for('index'))
+
+
+
+#this is a Mongodb database
+db = client.flask_database
+#this is a todos collection
+todos = db.todos
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
